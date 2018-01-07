0b32c5dcc3a7be9f37d5f8370867c8260dac7b18
jnhnd <jnhnd128@gmail.com>
2018-01-07 02:00:29 +0000

    oops

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,17 @@
 module Main where
 
+import           Data.Maybe
 import qualified Data.Text     as T
-import           Monaka.Markov (markovNodes, markovStr)
-import           Monaka.Poetry (findPoem)
+import           Monaka.Markov (markovNodes)
+import           Monaka.Poetry (findPoemFromNodes)
 import           Web.Twitter   (collectTweets, tweet)
 
 main :: IO ()
 main = do
     tweets <- collectTweets 500
-    return ()
-    -- let text = T.unlines tweets
-    -- markov <- markovStr (T.unpack text)
-    -- poem <- findPoem [5,7,5,7,7] markov
-    -- tweet $ T.pack poem
+    let text = T.unlines tweets
+    markov <- markovNodes text
+    let poem = findPoemFromNodes [5,7,5,7,7] markov
+    case poem of
+        Just p -> tweet p
+        _      -> error "短歌生成失敗;;"
